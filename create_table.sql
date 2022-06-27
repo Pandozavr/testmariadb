4ffd2aa1b61b5b5c63c4f86ed265ec068ecf9c20
Melnichenok Sergey <melnichenok.s@mara.local>
2022-06-27 22:36:09 +0300

    обновлённые данные для создания таблиц

--- create_table.sql
+++ create_table.sql
@@ -1,41 +1,56 @@
-CREATE TABLE mern.`user` (
-	user_id INT auto_increment NOT NULL,
-	email varchar(100) NOT NULL,
-	password varchar(100) NOT NULL,
-	CONSTRAINT user_pk PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb3
-COLLATE=utf8mb3_general_ci;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `isFriend` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3
 
 ////////////////////////////////////
 
-CREATE TABLE mern.`user_token` (
+CREATE TABLE `user_token` (
   `user_id` int(11) NOT NULL,
-  `refresh_token` varchar(100) NOT NULL,
+  `refresh_token` mediumtext NOT NULL,
   KEY `user_token_FK` (`user_id`),
   CONSTRAINT `user_token_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
 
 //////////////////////////////////////
 
-CREATE TABLE mern.user_avatar (
-	file_name varchar(100) NOT NULL,
-	user_id INT NOT NULL,
-	CONSTRAINT user_avatar_FK FOREIGN KEY (user_id) REFERENCES mern.`user`(user_id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb3
-COLLATE=utf8mb3_general_ci;
+CREATE TABLE `user_avatar` (
+  `file_name` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  KEY `user_avatar_FK` (`user_id`),
+  CONSTRAINT `user_avatar_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
 
 //////////////////////////////////////
 
-CREATE TABLE mern.user_post (
-	user_id INT NOT NULL,
-	post_id INT auto_increment NOT NULL,
-	post_text MEDIUMTEXT NOT NULL
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb3
-COLLATE=utf8mb3_general_ci;
+CREATE TABLE `user_post` (
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_text` mediumtext NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `user_post_FK` (`user_id`),
+  CONSTRAINT `user_post_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=utf8mb3
 
+//////////////////////////////////////
+
+CREATE TABLE `user_friend` (
+  `user_id` int(11) NOT NULL,
+  `user_friend_id` int(11) NOT NULL,
+  KEY `user_friend_FK` (`user_id`),
+  CONSTRAINT `user_friend_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+
+//////////////////////////////////////
+
+CREATE TABLE `music` (
+  `track_id` int(11) NOT NULL AUTO_INCREMENT,
+  `track_name` varchar(100) NOT NULL,
+  `artist` varchar(100) NOT NULL,
+  `file_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`track_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3
