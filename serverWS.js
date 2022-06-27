170092160d357f74b8ee573eaf68c016bfeb72c8
Melnichenok Sergey <melnichenok.s@mara.local>
2022-06-27 22:31:52 +0300

    доработка сокетов, теперь отдаются и получаются входяшие/уходящие юзеры

--- serverWS.js
+++ serverWS.js
@@ -1,4 +1,5 @@
 const ws = require("ws");
+const uuid = require("uuid");
 
 const wss = new ws.Server({
         port: 5000
@@ -6,10 +7,24 @@ const wss = new ws.Server({
 );
 
 let n = 0;
+const clientsArr = [];
+
+function exitUser(id) {
+    for(i=0; i < clientsArr.length; i++){
+        if(id === clientsArr[i].id){
+            clientsArr.splice(i,1)
+        }
+    }
+}
 
 wss.on("connection", function connection (ws) {
+
+    const id = uuid.v4();
+
     ws.on("message", function (message) {
         message = JSON.parse(message);
+        clientsArr.push({id: id, user: message.userName})
+        console.log(clientsArr);
         message.id = n++;
         switch (message.event) {
             case "message":
@@ -17,14 +32,25 @@ wss.on("connection", function connection (ws) {
                 break;
             case "connection":
                 broadcastMessage(message)
+                broadcastUserList(clientsArr)
                 break;
         }
     })
+    ws.on("close", (event) => {
+        exitUser(id)
+        broadcastUserList(clientsArr)
+        console.log(clientsArr);
+    }) 
 });
 
 
 function broadcastMessage(message) {
     wss.clients.forEach( client => {
         client.send(JSON.stringify(message))
+    })
+}
+function broadcastUserList(users) {
+    wss.clients.forEach( client => {
+        client.send(JSON.stringify(users))
     })
 }
