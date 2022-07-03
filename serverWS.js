2e36cdfe6b2c94c39bd9a7fc6e6259bc3c69770e
Melnichenok Sergey <melnichenok.s@mara.local>
2022-07-03 22:40:08 +0300

    доработка сообщений для чата

--- serverWS.js
+++ serverWS.js
@@ -3,7 +3,7 @@ const uuid = require("uuid");
 
 const wss = new ws.Server({
         port: 5000
-    }, () => console.log("Server started on port 5000")
+    }, () => console.log("Server started on port 5000_")
 );
 
 let n = 0;
@@ -17,14 +17,27 @@ function exitUser(id) {
     }
 }
 
+function getExitUser(id){
+    for(i=0; i < clientsArr.length; i++){
+        if(id === clientsArr[i].id){
+            return clientsArr[i].user
+        }
+    }
+}
+
 wss.on("connection", function connection (ws) {
 
     const id = uuid.v4();
 
     ws.on("message", function (message) {
         message = JSON.parse(message);
-        clientsArr.push({id: id, user: message.userName})
-        console.log(clientsArr);
+        if(message.event === "connection"){
+            clientsArr.push({id: id, user: message.userName })
+        }
+        const usersForSend = {
+            event: "users",
+            users: clientsArr
+        }
         message.id = n++;
         switch (message.event) {
             case "message":
@@ -32,14 +45,23 @@ wss.on("connection", function connection (ws) {
                 break;
             case "connection":
                 broadcastMessage(message)
-                broadcastUserList(clientsArr)
+                broadcastUserList(usersForSend)
                 break;
         }
     })
-    ws.on("close", (event) => {
+    ws.on("close", () => {
+        const name = getExitUser(id)
         exitUser(id)
-        broadcastUserList(clientsArr)
-        console.log(clientsArr);
+        const usersForSend = {
+            event: "users",
+            users: clientsArr
+        }
+        const userExit = {
+            event: "exit",
+            userName: name
+        }
+        broadcastUserList(usersForSend)
+        broadcastMessage(userExit)
     }) 
 });
 
